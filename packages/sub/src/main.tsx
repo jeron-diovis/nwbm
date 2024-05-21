91741d6f861093ac1baad9555706c1d6dcba0b22
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-05-21 17:40:12 +0300

    add local package dependency

--- packages/sub/src/main.tsx
+++ packages/sub/src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// example import via tsconfig.paths
+import Example1 from "@core/App.tsx"
+
+// example import via workspace (dependency in package.json)
+import Example2 from "@scope/core/src/App.tsx"
+
+console.log(Example1, Example2)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
