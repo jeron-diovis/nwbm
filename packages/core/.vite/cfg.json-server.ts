322e874bbcfccac5631567a2a17ead353fe1aa14
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-05-27 18:54:38 +0300

    vite/json-server: clean up; make api prefix configurable via env vars.

--- packages/core/.vite/cfg.json-server.ts
+++ packages/core/.vite/cfg.json-server.ts
@@ -1,81 +1,137 @@
-import * as sysPath from 'path'
+import sysPath from 'path'
 
+import { loadEnv } from 'vite'
 import mockDevServerPlugin, {
   MockOptions,
   createDefineMock,
 } from 'vite-plugin-mock-dev-server'
 import { defineChunk } from 'vite-split-config'
 
+import chalk from 'chalk'
+import { flow } from 'lodash-es'
+
 // ---
 
-const API_PREFIX = '/api'
+/* At the time this config file is read, env files are not loaded.
+ * So we can't use `process.env` directly. */
+const env = loadEnv(
+  'development' /* Assume json-server is only needed in dev mode anyway */,
+  '',
+  ''
+)
 
-// ---
+const DEFAULT_API_PREFIX = '/api'
 
-export const defineMock = createDefineMock(mock => {
-  if (!mock.url.startsWith('/')) {
-    mock.url = sysPath.join(API_PREFIX, mock.url)
+const ENV_KEY = 'USE_JSON_SERVER'
+const ENV_VAL_USE_DEFAULT = 'true'
+
+const ENV_VAL = env[ENV_KEY]
+const isServerEnabled = ENV_VAL !== undefined
+
+function resolveApiPrefix(): string[] {
+  if (!isServerEnabled) {
+    return []
   }
+  const str = ENV_VAL === ENV_VAL_USE_DEFAULT ? DEFAULT_API_PREFIX : ENV_VAL
+  return str.split('|')
+}
 
-  /* For non-WS mocks, default method is ['GET', 'POST']
-   * Which may be very unexpected, when your 'default' GET mock intercepts an explicitly specified POST mock.
-   * @see https://github.com/pengzhanbo/vite-plugin-mock-dev-server/blob/04d7c9bb13e8b2961271944c379aeb2f295a7a04/src/baseMiddleware.ts#L220
-   */
+// ---
+
+export const useJsonServer = defineChunk(() => {
+  const prefix = resolveApiPrefix()
+  logServerState(isServerEnabled, prefix)
+  if (isServerEnabled) {
+    return {
+      plugins: [
+        /** @see https://github.com/pengzhanbo/vite-plugin-mock-dev-server#options */
+        mockDevServerPlugin({
+          prefix,
+          log: 'info',
+        }),
+      ],
+    }
+  }
+})
+
+// ---
+
+function logServerState(enabled: boolean, prefix: string[]) {
+  const filepath = sysPath.relative(process.cwd(), __filename)
+  const tag = chalk.blueBright(`[${filepath}]`)
+  if (enabled) {
+    console.log(
+      `${tag} mock-server ${chalk.green('enabled')}. Mocking requests starting with: ${prefix.map(x => chalk.cyan(x)).join(' , ')}`
+    )
+  } else {
+    console.log(
+      `${tag} mock-server ${chalk.red('disabled')}.
+To enable data mocking, you may add one of the following env variables:
+* ${chalk.blue(`${ENV_KEY}=${ENV_VAL_USE_DEFAULT}`)} – to intercept requests to default ${chalk.cyan(DEFAULT_API_PREFIX)} path
+* ${chalk.blue(`${ENV_KEY}=/prefix1|/prefix2|...`)} – to intercept requests to custom paths
+`
+    )
+  }
+}
+
+// ---
+
+type Mock = MockOptions[number]
+type MockTransform = (mock: Mock) => Mock
+
+const createMockTransform =
+  (transform: (mock: Mock) => Mock | void): MockTransform =>
+  mock =>
+    transform(mock) ?? mock
+
+/**
+ * Prepend api paths prefix to created mocks
+ */
+export const createTransformPathPrefix: (
+  prefix: string
+) => MockTransform = prefix =>
+  createMockTransform(mock => {
+    if (!mock.url.startsWith('/')) {
+      mock.url = sysPath.join(prefix, mock.url)
+    }
+  })
+
+export const transformPathPrefix = createTransformPathPrefix(DEFAULT_API_PREFIX)
+
+/**
+ * For non-WS mocks, default method is ['GET', 'POST']
+ * Which may be very unexpected, when your 'default' GET mock intercepts an explicitly specified POST mock.
+ * @see https://github.com/pengzhanbo/vite-plugin-mock-dev-server/blob/04d7c9bb13e8b2961271944c379aeb2f295a7a04/src/baseMiddleware.ts#L220
+ */
+export const transformGetOnly = createMockTransform(mock => {
   if (!('ws' in mock) && !('method' in mock)) {
     mock.method = 'GET'
   }
-
-  enableAutoStringify(mock)
 })
 
 /**
  * Automatically `JSON.stringify` objects and arrays passed to `response.end()`
  */
-function enableAutoStringify(mock: MockOptions[0]) {
+export const transformStringifyResponse = createMockTransform(mock => {
   if ('response' in mock && mock.response !== undefined) {
+    const maybeStringify = (arg: unknown) =>
+      arg?.constructor === Object || arg?.constructor === Array
+        ? JSON.stringify(arg)
+        : arg
+
     const { response } = mock
     mock.response = function (req, res, next) {
       const { end } = res
-      res.end = function (this: typeof res, arg, ...rest) {
-        const chunk =
-          arg?.constructor === Object || arg?.constructor === Array
-            ? JSON.stringify(arg)
-            : arg
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return end.call(this, chunk, ...(rest as [any, any]))
-      } as typeof end
+      // @ts-expect-error Exact types are unnecessarily cumbersome here.
+      res.end = function (arg, ...rest) {
+        return end.apply(this, [maybeStringify(arg), ...rest] as any) // eslint-disable-line @typescript-eslint/no-explicit-any
+      }
 
       return response(req, res, next)
     }
   }
-  return mock
-}
-
-// ---
-
-const ENV_KEY = 'USE_JSON_SERVER'
-const ENV_VAL = 'true'
-
-/** @see https://github.com/pengzhanbo/vite-plugin-mock-dev-server#options */
-const PLUGIN = mockDevServerPlugin({
-  prefix: API_PREFIX,
-  log: 'info',
 })
 
-export const useJsonServer = defineChunk((_, { env }) => {
-  const enabled = env[ENV_KEY] === ENV_VAL
-  logServerState(enabled)
-  return enabled ? { plugins: [PLUGIN] } : undefined
-})
-
-// ---
-
-function logServerState(enabled: boolean) {
-  if (enabled) {
-    console.log(`[json-server] Server enabled. Mocking requests: ${API_PREFIX}`)
-  } else {
-    console.log(
-      `[json-server] Server disabled. To enable data mocking, add env variable '${ENV_KEY}=${ENV_VAL}'`
-    )
-  }
-}
+export const defineMock = createDefineMock(
+  flow(transformPathPrefix, transformGetOnly, transformStringifyResponse)
+)
