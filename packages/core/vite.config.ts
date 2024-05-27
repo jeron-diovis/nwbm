66a12bd212f3040449a705c25e64f289062e9c5d
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-05-27 16:56:48 +0300

    Bring back alias for `~mock` folder, as it can't be handled by tsconfig "paths" option

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
     /** Note these aliases imply css files too – affecting paths in `composes` prop. */
     alias: {
       src: sysPath.resolve(__dirname, 'src'),
+
+      /* Handy alias to navigate a vast nested mocks structure.
+       * Supposed to be used _only_ inside '/mock' folder.
+       * DO NOT import it in app sources. */
+      '~mock': sysPath.resolve(__dirname, 'mock'),
     },
   },
 })
