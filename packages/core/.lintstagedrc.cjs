937feb0b969ac35b6464fdf23f0a5c4b6fb5239d
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-05-27 09:51:38 +0300

    fix typo

--- packages/core/.lintstagedrc.cjs
+++ packages/core/.lintstagedrc.cjs
@@ -1,5 +1,5 @@
 /* Config in a package MUST exist.
- * lint-staged will for a config _closest to the staged file being linted_.
+ * lint-staged will search for a config _closest to the staged file being linted_.
  * @see https://www.npmjs.com/package/lint-staged#how-to-use-lint-staged-in-a-multi-package-monorepo */
 
 const { createDefaultConfig } = require('../../.lintstagedrc.scripts.cjs')
