3d11fc881fd73e7a5e254de3b1d8e140017cbdab
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-07-21 14:39:22 +0300

    use other plugin for bundle analysis

--- .vite/index.ts
+++ .vite/index.ts
@@ -1,6 +1,5 @@
 import { useChunks } from 'vite-split-config'
 
-import { useBuildAnalysis } from './cfg.build-analysis'
 import { useBuildLib } from './cfg.build-lib'
 import { useLint } from './cfg.lint'
 import { useModularImports } from './cfg.modular-imports'
@@ -8,20 +7,11 @@ import { useReact } from './cfg.react'
 import { useCSS } from './cfg.styles'
 import { useTests } from './cfg.tests'
 
-export {
-  useReact,
-  useLint,
-  useModularImports,
-  useCSS,
-  useTests,
-  useBuildAnalysis,
-  useBuildLib,
-}
+export { useReact, useLint, useModularImports, useCSS, useTests, useBuildLib }
 
 export const defineConfig = useChunks([
   useReact,
   useModularImports,
   useTests,
-  useBuildAnalysis,
   useCSS,
 ])
