e8d84902ed5106ffe0f70ff883eb9ff199d0941f
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-08-02 07:55:02 +0300

    [dirty] cleanup types

--- packages/use-event/src/useDomEvent.types.ts
+++ packages/use-event/src/useDomEvent.types.ts
@@ -13,48 +13,24 @@ export type Target =
   | RefOrVal<HTMLElement>
   | null
 
-type Hack<A, B> = Record<Exclude<keyof A, keyof B>, never>
-type XHack<A, B> = A & Hack<A, B>
-type Arg<T> = T extends (x: infer A) => unknown ? A : never
-
 export interface IUseDomEvent {
-  /*
-   * filter - ok
-   * hint - FAIL (string props)
-   * random prop – perfect (highlight precisely, "does not exist in type...")
-   * array prop - meh (error on overload level, "does not exist in type...")
-   */
-  /*<T extends Target, E extends keyof GetMap<T>>(
+  <T extends Target, M extends ListenersMap<GetEventsMap<T>>>(
     el: T,
-    events: {
-      [K in E]?: Listener<GetMap<T>[K]>
-    },
-    options?: UseDomEventOptions<GetMap<T>[E]>
-  ): void*/
-
-  /*
-   * filter - ok
-   * hint - ok (only String.iterator, tolerable)
-   * random prop – ok (highlight precisely, "not assignable to never")
-   * array prop - meh (error on overload level,  "not assignable to never")
-   */
-  <T extends Target, M extends XMap<GetMap<T>>>(
-    el: T,
-    events: XHack<M, GetMap<T>>,
-    options?: UseDomEventOptions<Arg<M[keyof M]>>
+    events: Listeners<M, GetEventsMap<T>>,
+    options?: UseDomEventOptions<GetFirstArg<M[keyof M]>>
   ): void
 
-  <T extends Target, E extends keyof GetMap<T>>(
+  <T extends Target, E extends keyof GetEventsMap<T>>(
     el: T,
     event: E | E[],
-    callback: Listener<GetMap<T>[E]>,
-    options?: UseDomEventOptions<GetMap<T>[E]>
+    callback: Listener<GetEventsMap<T>[E]>,
+    options?: UseDomEventOptions<GetEventsMap<T>[E]>
   ): void
 }
 
 // ---
 
-type GetMap<T> = T extends 'window'
+type GetEventsMap<T extends Target> = T extends 'window'
   ? WindowEventMap
   : T extends 'document'
     ? DocumentEventMap
@@ -62,9 +38,9 @@ type GetMap<T> = T extends 'window'
       ? NormalizedVisualViewportEventMap
       : HTMLElementEventMap
 
-type XMap<M> = EventMap<{
-  [K in keyof M]?: Listener<M[K]>
-}>
+type ListenersMap<EventsMap> = {
+  [E in keyof EventsMap]?: Listener<EventsMap[E]>
+}
 
 type Listener<E> = (event: E) => void
 
@@ -94,17 +70,12 @@ type Overwrite<T extends object, O extends { [K in keyof T]?: unknown }> = Omit<
 > &
   O
 
-/* A hack to improve a bit an intellisense for object literals.
- * Because of overloaded function interface, where one option uses objects and other one uses arrays,
- * typescript is confused when trying to suggest props for an object version.
- * Array is object too – so suggested keys include all array instance props,
- * which is super confusing for a user.
- * So we explicitly exclude all of those.
+type GetFirstArg<T> = T extends (x: infer A) => unknown ? A : never
+
+/*
+ * A hack to make excess props validation work for generics.
+ * @see https://stackoverflow.com/a/78798427/3437433
  *
- * This will NOT help when you already provided an invalid key to an object –
- * in that case, TS doesn't understand at all what kind of object is that,
- * and will suggest everything it knows, regardless of this hack.
- * But aside from that case, it really helps to clean up suggestions.
- *
- * Type is abstractly named `EventMap` just for it to look nice in TS hint and error messages. */
-type EventMap<T> = Omit<T, keyof unknown[] | keyof string>
+ * Name it `Listeners` just to make it look nice in TS error messages.
+ */
+type Listeners<A, B> = A & Record<Exclude<keyof A, keyof B>, never>
